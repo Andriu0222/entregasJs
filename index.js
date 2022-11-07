be9de18ac821c109876dfed308519a191e8013f9
Andriu0222 <andres.felipe.higuita@outlook.com>
2022-11-07 16:43:39 -0500

    segunda entrga simulador interactivo

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-const precioSalpiconHelado = 7000;
-const precioConoUnaBola = 1500;
-const precioEnsaladaFrutas = 8000;
-const precioBrownieHelado = 10000;
-const precioMalteada = 6000;
+const helados = [];
+class Helado {
+    constructor(id, name, price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+}
+
+const heladoUno = new Helado(1, 'Salpicon con helado', 7000);
+helados.push(heladoUno);
+const heladoDos = new Helado(2, 'Cono con una bola', 1500);
+helados.push(heladoDos);
+const heladoTres = new Helado(3, 'Ensalada de frutas', 8000);
+helados.push(heladoTres);
+const heladoCuatro = new Helado(4, 'Brownie con helado', 10000);
+helados.push(heladoCuatro);
+const heladoCinco = new Helado(5, 'Malteada', 6000);
+helados.push(heladoCinco);
 
 let flag;
 let total = 0;
@@ -20,8 +34,8 @@ do {
             3. Vainilla
             4. Vainilla chips
             5. Cafe
-            6. Chocolate           
-            `)
+            6. Chocolate
+             `)
             return saborHeladoUno;
         }
 
@@ -32,14 +46,14 @@ do {
             2. Fresa
             3. Arequipe
             4. Vainilla
-            5. Cafe           
-            `)
+            5. Cafe
+             `)
             return saborMalteada;
         }
 
         pedidoInicial = prompt(`Ingresa la opcion que quieras comprar
         1. Salpicon con Helado
-        2. Cono con una bola        
+        2. Cono con una bola
         3. Ensalada de frutas
         4. Brownie con helado
         5. Malteada        `);
@@ -47,27 +61,27 @@ do {
         switch (pedidoInicial) {
             case "1":
                 saborHeladoUno = escogerHelado();
-                total = total + precioSalpiconHelado;
+                total = total + heladoUno.price;
                 console.log("El total a pagar es", total);
                 break;
             case "2":
                 saborHeladoUno = escogerHelado();
-                total = total + precioConoUnaBola;
+                total = total + heladoDos.price;
                 console.log("El total a pagar es", total);
                 break;
             case "3":
                 saborHeladoUno = escogerHelado();
-                total = total + precioEnsaladaFrutas;
+                total = total + heladoTres.price;
                 console.log("El total a pagar es", total);
                 break;
             case "4":
                 saborHeladoUno = escogerHelado();
-                total = total + precioBrownieHelado;
+                total = total + heladoCuatro.price;
                 console.log("El total a pagar es", total);
-                break;               
+                break;
             case "5":
                 saborMalteada = escogerMalteada();
-                total = total + precioMalteada;
+                total = total + heladoCinco.price;
                 console.log("El total a pagar es", total);
                 break;
             default:
@@ -83,6 +97,6 @@ do {
             opcionErrorHelado = false;
         }
     }
-    flag = prompt ("Quieres hacer otro pedido ? S/N")
+    flag = prompt("Quieres hacer otro pedido ? S/N")
 
 } while (flag == "S" || flag == "s")
